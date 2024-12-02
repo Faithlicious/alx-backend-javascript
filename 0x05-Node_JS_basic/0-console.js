d206587ea58803f26ddffa1229fef8939b84052e
Faithlicious <violetshiko15@gmail.com>
2024-12-02 17:09:28 +0000

    0x05-Node_JS_basic

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,10 @@
-const displayMessage = (str) => console.log(str);
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Faith kamau <https://github.com/Faithlicious>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;
